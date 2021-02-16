3f975e9c8a0f7cb0a57d5e14efd7584744bbfc70
Céline Bossuet <Celine.Bossuet@grenoble-inp.org>
2021-02-16 21:10:20 +0100

    essai correction main_copy mais bon...

--- main_copy.py
+++ main_copy.py
@@ -22,23 +22,38 @@ def load_instance(filename):
 
     return distance, points
 
+def fusion (J,L):
+    for l in L:
+        J.append(l)
+    L=[]
+
+def recherche(L,M):
+    for l in L:
+        for m in range (0,len(M)):
+            if l==M[m]: 
+                
+                return True
+    return False
 
 def graphe(distance, points):
-    convexe=[[points[0]]]
+    convexe=[[pts] for pts in points]
     for i in range (1, len(points)):
-        boucle=True
-        bcl=True
         j=0
-        while j <len(convexe) and boucle:
-            k=0
-            while k < len(convexe[j])  and bcl:
+        while j <i:
+            k,l=0,j+1
+            while l<i : #si dans 2 convexes il y a le même point on les concatene
+                if convexe[l]!=[] and convexe[j]!=[]:    
+                    if recherche(convexe[j], convexe[l]):
+                        fusion(convexe[j], convexe[l][:-1])
+                        break
+                l+=1
+            while k < len(convexe[j]): #on regarde dans chaque convexe déjà fait si i est voisin a un point
                 if Point.distance_to(points[i], convexe[j][k])<=distance:
                     convexe[j].append(points[i])
-                    bcl,boucle=False, False
+                    convexe[i]=[]
+                    break
                 k+=1
             j+=1
-        if boucle:
-            convexe.append([points[i]])
     return convexe
 
 

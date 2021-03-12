b70caa1c1365cc519de1c8437d1b6486ee5d3321
Céline Bossuet <Celine.Bossuet@grenoble-inp.org>
2021-03-12 15:57:57 +0100

    changement de main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-Même raisonnement que la première version mais de manière itérative
+compute sizes of all connected components.
+sort and display.
 """
-#from timeit import timeit
-from sys import argv
+
 from time import time
+from sys import argv
 from geo.point import Point
 
 
@@ -20,49 +21,46 @@ def load_instance(filename):
 
     return distance, points
 
-def construction_graphe(distance, points):
-    """
-    Fonction permettant de constuire un graphe à partir des données du problèmes.
-    Ce graphe se construit en considérant voisin un point seulement ses voisins
-    d'indices plus grand que lui-même.
-    """
-    liste_voisins=[[] for i in range(len(points))]
-    for i in range(len(points)):
-        for j in range(i+1,len(points)):
-            if Point.distance_to(points[i], points[j]) <= distance:
-                liste_voisins[i].append(j)
-                liste_voisins[j].append(i)
-    return liste_voisins
 
-def construction_tableau_classe_iter(liste_voisin):
+def graphe(distance, points):
+    """ fonction qui construit mes sous graphes au fur et à mesure
     """
-    DFS simple sur la liste de voisins
-    """
-    liste=[]
-    tableau_marque=[False for i in range(len(liste_voisin))]
-    for i in range(len(liste_voisin)):
-        if not tableau_marque[i]:
-            stack=[i]
-            compteur=0
-            while stack!=[]:
-                sommet_traité=stack.pop()
-                if not tableau_marque[sommet_traité]:
-                    tableau_marque[sommet_traité]= True
-                    compteur+=1
-                    stack+= liste_voisin[sommet_traité]
-            liste.append(compteur)
-    return liste
-
+    convexe=[[points[0]]]
+    for i in range (1, len(points)):
+        boucle=True
+        j=0
+        l=[]
+        while j <len(convexe):
+            k=0
+            while k < len(convexe[j]):
+                if Point.distance_to(points[i], convexe[j][k])<=distance:
+                    l.append(j)
+                    boucle=False
+                    break
+                k+=1
+            j+=1
+        if not boucle:
+            convexe[l[0]].append(points[i])
+        while len(l)>1:
+            convexe[l[0]]+= convexe[l[-1]]
+            convexe[-1], convexe[l[-1]] = convexe[l[-1]], convexe[-1]
+            l.pop()
+            convexe.pop()
+        if boucle:
+            convexe.append([points[i]])
+    return convexe
 
 
 def print_components_sizes(distance, points):
     """
     affichage des tailles triees de chaque composante
     """
-    graphe=construction_graphe(distance, points)
-    classe=construction_tableau_classe_iter(graphe)
-    print(sorted(classe,reverse=True))
-
+    graph=graphe(distance,points)
+    L=[]
+    for g in graph:
+        if len(g)!=0:
+            L.append(len(g))
+    print(sorted(L, reverse=True))
 
 
 def main():
@@ -72,6 +70,5 @@ def main():
     for instance in argv[1:]:
         distance, points = load_instance(instance)
         print_components_sizes(distance, points)
-
 
 main()

52869948771f29ea3044aa297e286b2b57ca3798
lecampt <titouan.lecamp@grenoble-inp.org>
2021-03-14 16:34:02 +0100

    Enfin une version efficace?

--- version8.py
+++ version8.py
@@ -47,16 +47,25 @@ def est_connexe(case1, case2, distance):
     return False
 
 
-def actualiser_classe( i, j, nb):
+def trouver_borne( i, j, nb):
     min = None
     i_max = i + 2
     j_min = j - 2
     j_max = j + 2 
     if i_max > nb - 2:
         i_max = i
-    if i_max > nb - 1: 
+    elif i_max > nb - 1: 
         i_max = i + 1 
-    if 
+    if j_min < -1:
+        j_min= j
+    elif j_min < 0:
+        j_min= j-1
+    if j_max > nb-2:
+        j_max = j
+    elif j_max > nb-1:
+        j_max = j+1
+    return i_max, j_min, j_max
+
     
     
     
@@ -64,15 +73,42 @@ def actualiser_classe( i, j, nb):
             
 
 
-def construit_table_équivalence(matrice_point, maille):
+def construit_table_équivalence(matrice_point, maille, distance):
     nb = int(1/maille) + 1
-    l=[]
+    liste_classe={}
     tableau_équiv = [ [None for _ in range(nb)] for _ in range(nb)]
-    nouveau = 0 
+    nouveau = 0
     for i in range(nb):
         for j in range(nb):
             if matrice_point[i][j] != []:
-                
+                i_max, j_min, j_max = trouver_borne(i, j, nb)
+                attente={}
+                for k in range(i, i_max + 1):
+                    for l in range(j_min, j_max + 1):
+                        if matrice_point[k][l] != []:
+                            if est_connexe(matrice_point[i][j], matrice_point[k][l], distance):
+                                attente[(k,l)]= tableau_équiv[k][l]
+                classe = nouveau
+                for clé, values in attente.items():
+                    if values is not None  and values < nouveau :
+                        classe=min(classe, values)
+                tableau_équiv[i][j]=classe
+                for clé , values in attente.items():
+                    tableau_équiv[clé[0]][clé[1]]= classe
+                if classe == nouveau:
+                    nouveau+=1
+    for i in range(nb):
+        for j in range(nb):
+            if tableau_équiv[i][j] is None:
+                pass
+            elif tableau_équiv[i][j] not in liste_classe:
+                liste_classe[tableau_équiv[i][j]]= len(matrice_point[i][j])
+            else:
+                liste_classe[tableau_équiv[i][j]] += len(matrice_point[i][j])
+    liste= []
+    for clé, values in liste_classe.items():
+        liste.append(values)
+    return liste
 
 
 
@@ -81,7 +117,9 @@ def print_components_sizes(distance, points):
     """
     affichage des tailles triees de chaque composante
     """
-    pass #TO DO
+    maille = maillage(distance)
+    matrice_point= construction_matrice(maille, points)
+    print(sorted(construit_table_équivalence(matrice_point, maille, distance), reverse=True))
 
 
 def main():
